8e3cef076f5a88858ae6bc2ab3e8927863dd30ef
Aqdas Zishan <97790386+AqdasZishan@users.noreply.github.com>
2025-05-20 23:03:27 +0530

    Delete backend directory

--- backend/backend.js
+++
@@ -1,2 +0,0 @@
-const backend = "http://localhost:3000";
-export default backend;
--- backend/prisma/migrations/20250519183029_add_profile_picture/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "_Student" ADD CONSTRAINT "_Student_AB_pkey" PRIMARY KEY ("A", "B");
-
--- DropIndex
-DROP INDEX "_Student_AB_unique";
